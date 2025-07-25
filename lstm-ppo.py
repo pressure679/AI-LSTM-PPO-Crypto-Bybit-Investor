2d38d46e567e190b6710c1dd445e2b5f7a592f0f
Vittus Mikiassen <vittusmpress@yahoo.com>
2025-07-26 00:04:03 +0200

    Update lstm-ppo.py

--- lstm-ppo.py
+++ lstm-ppo.py
@@ -29,10 +29,8 @@ ACTIONS = ['hold', 'long', 'short', 'close']
 # gamma = 0.95
 # epsilon = 0.1
 
-# Bybit Demo API Key and Secret - K2OW1k9LlnjeQWYHUK - y3TZKS6KV6Yt5Y4SqdmN6NO8y6htZXiAmUeV
-# Bybit API Key and Secret - PoP1ud3PuWajwecc4S - z9RXVMWpiOoE3TubtAQ0UtGx8I5SOiRp1KPU
-api_key = "PoP1ud3PuWajwecc4S"
-api_secret = "z9RXVMWpiOoE3TubtAQ0UtGx8I5SOiRp1KPU"
+api_key = ""
+api_secret = ""
 session = HTTP(
     api_key=api_key,
     api_secret=api_secret,
