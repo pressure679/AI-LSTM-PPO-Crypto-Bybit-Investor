677a127572ad3f0acffffacb01b5cf9821385ca0
Vittus Mikiassen <vittusmpress@yahoo.com>
2025-08-03 15:15:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ git clone https://github.com/pressure679/AI-LSTM-PPO-Crypto-Bybit-Investor
 cd AI-LSTM-PPO-Crypto-Bybit-Investor
 mkdir LSTM-PPO-saves
 mv 2025-08-03-XAUUSD.win_rate_knn.pkl LSTM-PPO-saves
-mv 025-08-03-XAUUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 025-08-03-XRPUSD.win_rate_knn.pkl LSTM-PPO-SAVES
-mv 025-08-03-XRPUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 025-08-03-BNBUSD.win_rate_knn.pkl LSTM-PPO-SAVES
-mv 025-08-03-BNBUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 025-08-03-ETHUSD.win_rate_knn.pkl LSTM-PPO-SAVES
-mv 025-08-03-ETHUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 025-08-03-BTCUSD.win_rate_knn.pkl LSTM-PPO-SAVES
-mv 025-08-03-BTCUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv 2025-08-03-XAUUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv 2025-08-03-XRPUSD.win_rate_knn.pkl LSTM-PPO-SAVES
+mv 2025-08-03-XRPUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv 2025-08-03-BNBUSD.win_rate_knn.pkl LSTM-PPO-SAVES
+mv 2025-08-03-BNBUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv 2025-08-03-ETHUSD.win_rate_knn.pkl LSTM-PPO-SAVES
+mv 2025-08-03-ETHUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv 2025-08-03-BTCUSD.win_rate_knn.pkl LSTM-PPO-SAVES
+mv 2025-08-03-BTCUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
