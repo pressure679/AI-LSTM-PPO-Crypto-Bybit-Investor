4b62a4f3850a73ea2d57bb2a493a05090fcd9aa6
Vittus Mikiassen <vittusmpress@yahoo.com>
2025-08-07 22:45:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ python3 -m pip install pybit scikit-learn pandas numpy
 git clone https://github.com/pressure679/AI-LSTM-PPO-Crypto-Bybit-Investor
 cd AI-LSTM-PPO-Crypto-Bybit-Investor
 mkdir LSTM-PPO-saves
-mv 2025-08-07-XAUUSD.win_rate_knn.pkl LSTM-PPO-saves
-mv 2025-08-07-XAUUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 2025-08-07-XRPUSD.win_rate_knn.pkl LSTM-PPO-saves
-mv 2025-08-07-XRPUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 2025-08-07-BNBUSD.win_rate_knn.pkl LSTM-PPO-saves
-mv 2025-08-07-BNBUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 2025-08-07-ETHUSD.win_rate_knn.pkl LSTM-PPO-saves
-mv 2025-08-07-ETHUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
-mv 2025-08-07-BTCUSD.win_rate_knn.pkl LSTM-PPO-saves
-mv 2025-08-07-BTCUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv *XAUUSD.win_rate_knn.pkl LSTM-PPO-saves
+mv *XAUUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv *XRPUSD.win_rate_knn.pkl LSTM-PPO-saves
+mv *XRPUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv *BNBUSD.win_rate_knn.pkl LSTM-PPO-saves
+mv *BNBUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv *ETHUSD.win_rate_knn.pkl LSTM-PPO-saves
+mv *ETHUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
+mv *BTCUSD.win_rate_knn.pkl LSTM-PPO-saves
+mv *BTCUSD.checkpoint.lstm-ppo.pkl LSTM-PPO-saves
